0f48a533224946c73916eb53e9d65f01aa1cb116
Aalison <karilloret@hotmail.com>
2016-12-06 18:38:55 +0100

    mk

--- JavaScript/todo.js
+++ JavaScript/todo.js
@@ -1,22 +1,6 @@
 function get_todos() {
     var todos = new Array;
     var todos_str = localStorage.getItem('todo');
-    if (todos_str != null) {
-        todos = JSON.parse(todos_str); 
-    }
-    return todos;
-}
-function add() {
-    var task = document.getElementById('task').value;
-
-    var todos = get_todos();
-    todos.push(task);
-    localStorage.setItem('todo', JSON.stringify(todos));
-
-    show();
-function get_todos() {
-    var todos = new Array;
-    var todos_str = localStorage.getItem('todo');
     if (todos_str !== null) {
         todos = JSON.parse(todos_str); 
     }
@@ -65,21 +49,3 @@ function show() {
  
 document.getElementById('add').addEventListener('click', add);
 show();
-    return true;
-}
-function show() {
-    var todos = get_todos();
-
-    var html = '<ul>';
-    for(var i=0; i<todos.length; i++) {
-        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
-    };
-    html += '</ul>';
-
-    document.getElementById('todos').innerHTML = html;
-
-    var buttons = document.getElementsByClassName('remove');
-    for (var i=0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', remove);
-    };
-}
